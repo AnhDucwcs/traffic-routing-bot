60cd4ca76544d3b78b97100719f176f193e96df6
AnhDucwcs <lnanhduc12@gmail.com>
2026-05-09 13:27:14 +0700

    fix: dùng proxy Vietnam tránh lỗi bị chặn khi gọi api

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     TELEGRAM_BOT_TOKEN: str
     BUS_API_BASE_URL: str
     MONGO_URI: str
+    VN_PROXY: str
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
         
 settings = Settings()
