649f367ea111e1ad72561e971f4c7eb1ecd7e10c
AnhDucwcs <lnanhduc12@gmail.com>
2026-05-16 10:39:43 +0700

    chore: chuẩn bị đưa nó lên hugging face spaces

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,6 @@ class Settings(BaseSettings):
 
     PROJECT_NAME: str = "Traffic Routing Bot"
     TELEGRAM_BOT_TOKEN: str
-    BUS_API_BASE_URL: str
     MONGO_URI: str
     VN_PROXY: str
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
