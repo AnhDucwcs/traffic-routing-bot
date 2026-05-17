fb6cc17460879f4aaac73f71ece9e41cb1b86d48
AnhDucwcs <lnanhduc12@gmail.com>
2026-05-17 17:30:45 +0700

    feat: thêm nhịp nghỉ trong lúc cào

--- app/services/crawler/bus_crawler.py
+++ app/services/crawler/bus_crawler.py
@@ -172,7 +172,7 @@ class BusCrawler:
             client_kwargs["proxy"] = proxy_url
 
         async with httpx.AsyncClient(**client_kwargs) as http_client:
-            consumers = [asyncio.create_task(self.consumer_api_2(i, queue, http_client, all_results)) for i in range(20)]
+            consumers = [asyncio.create_task(self.consumer_api_2(i, queue, http_client, all_results)) for i in range(10)]
             total_stops = len(stop_ids)
             completed = 0
             next_log_pct = 10.0    
@@ -180,6 +180,7 @@ class BusCrawler:
             producer_tasks = [asyncio.create_task(self.producer_api_1(sid, queue, semaphore, http_client)) for sid in stop_ids]
             for task in asyncio.as_completed(producer_tasks):
                 await task
+                await asyncio.sleep(0.05)
                 completed += 1
                 progress_pct = (completed / total_stops) * 100 if total_stops else 100.0
 
