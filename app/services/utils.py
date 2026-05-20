ac6be74d91a5725f289d80833b082a800319a9d8
AnhDucwcs <lnanhduc12@gmail.com>
2026-05-20 17:52:22 +0700

    fix: sửa lại hàm _success_response khi thay đổi tham số

--- app/services/utils.py
+++ app/services/utils.py
@@ -4,5 +4,5 @@ def _error_response(message):
     return RoutingResponse(status="error", message=message)
 def _pending_response(message):
     return RoutingResponse(status="pending", message=message)
-def _success_response(message, url = None, distance_km = None, estimated_time_min = None, geojson = None, map_image_url = None):
-    return RoutingResponse(status="success", message=message, navigation_url=url, distance_km=distance_km, estimated_time_min=estimated_time_min, geojson=geojson, map_image_url=map_image_url)
+def _success_response(message, url = None, distance_km = None, estimated_time_min = None, geojson = None, route_id = None):
+    return RoutingResponse(status="success", message=message, navigation_url=url, distance_km=distance_km, estimated_time_min=estimated_time_min, geojson=geojson, route_id=route_id)
