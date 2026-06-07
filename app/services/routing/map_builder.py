8639f39551be36fad57fb958def6df5497495593
AnhDucwcs <lnanhduc12@gmail.com>
2026-06-07 15:27:43 +0000

    feat: thêm hàm tải route_stop_sequence lên ram fix: sửa lại đuôi file segment_lengths_v2.json

--- app/services/routing/map_builder.py
+++ app/services/routing/map_builder.py
@@ -28,10 +28,23 @@ def load_segment_lengths():
     
     curent_dir = Path(__file__).resolve().parent
     src_dir = curent_dir.parent.parent.parent
-    lengths_path = src_dir / "data" / "segment_lengths_v2.pkl"
+    lengths_path = src_dir / "data" / "segment_lengths_v2.json"
     
     with open(lengths_path, 'r', encoding='utf-8') as f:
             segment_lengths = json.load(f)
     
     logger.info(f"Segment lengths loaded.")
     return segment_lengths
+
+def load_route_stop_sequence():
+    logger.info("Loading route stop sequence from disk...")
+    
+    curent_dir = Path(__file__).resolve().parent
+    src_dir = curent_dir.parent.parent.parent
+    sequence_path = src_dir / "data" / "route_stop_sequence.json"
+    
+    with open(sequence_path, 'r', encoding='utf-8') as f:
+            route_stop_sequence = json.load(f)
+    
+    logger.info(f"Route stop sequence loaded.")
+    return route_stop_sequence
